e243af2e8121f7d679835a15cdea66bd92245a78
Andela Developer <andela@macs-MacBook-Pro.local>
2015-03-11 08:35:21 +0100

    news girl live

--- js/textController.js
+++ js/textController.js
@@ -19,8 +19,52 @@ textApp.controller('textController', ['$scope', 'myFactory', 'myFactory1', funct
     value: 't'
   }];
 
+  var query = 0;
+
+
   $scope.getValue = function(value) {
-    myFactory.getResponse(value, callback);
+    $scope.value = value;
+    emptyArray = [];
+    query = 0;
+    // $scope.display = 
+    if(value === 'p'){
+      $scope.display = 'Politics';
+
+    }
+
+    else if(value === 'w'){
+      $scope.display = 'World News';
+
+    }
+    else if(value === 'b'){
+      $scope.display = 'Business News';
+
+    }
+
+    else if(value === 'h'){
+      $scope.display = 'Headlines';
+
+    }
+
+    else if(value === 't'){
+      $scope.display = 'Science and Technology';
+
+    }
+
+    myFactory.getResponse(value, query, callback);
+
+  };
+  
+  $scope.getValue1 = function(){
+    if (query < 63) {
+      query = query + 8;
+
+    } else {
+      query = 0;
+    }
+    // console.log(query);
+     myFactory.getResponse($scope.value, query, callback);
+
   };
 
    myFactory1.getResponse1(function(data) {
--- js/textService.js
+++ js/textService.js
@@ -1,9 +1,9 @@
 var myService = angular.module('myService', []);
 myService.factory('myFactory', ['$http', function($http) {
   return {
-    getResponse: function(search, callback) {
+    getResponse: function(search, query, callback) {
       console.log(search);
-      return $http.jsonp("https://ajax.googleapis.com/ajax/services/search/news?v=1.0&topic=" + search + "&callback=JSON_CALLBACK&rsz=8")
+      return $http.jsonp("https://ajax.googleapis.com/ajax/services/search/news?v=1.0&topic=" + search + "&callback=JSON_CALLBACK&rsz=8&start=" + query)
         .success(callback)
         .error(function(error) {
           console.log(error);
