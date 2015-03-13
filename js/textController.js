e4505761751327a5ef6c7bb8979a7cffc3ef3a14
Andela Developer <andela@macs-MacBook-Pro.local>
2015-03-13 05:13:10 +0100

    Updated menu bar and the rest

--- js/textController.js
+++ js/textController.js
@@ -1,7 +1,9 @@
 var textApp = angular.module('textApp', ['myService', 'myService1']);
 textApp.controller('textController', ['$scope', 'myFactory', 'myFactory1', function($scope, myFactory, myFactory1) {
-	var emptyArray = [];
-
+  var emptyArray = [];
+  var query = 0;
+  $scope.value = 'p';
+  myFactory.getResponse($scope.value, query, callback);
   $scope.list = [{
     name: 'Politics',
     value: 'p'
@@ -18,35 +20,26 @@ textApp.controller('textController', ['$scope', 'myFactory', 'myFactory1', funct
     name: 'Science and Technology',
     value: 't'
   }];
-
-  var query = 0;
-
-
+  
   $scope.getValue = function(value) {
     $scope.value = value;
     emptyArray = [];
     query = 0;
+    $scope.show1 = '';
     // $scope.display = 
-    if(value === 'p'){
+    if (value === 'p') {
       $scope.display = 'Politics';
 
-    }
-
-    else if(value === 'w'){
+    } else if (value === 'w') {
       $scope.display = 'World News';
 
-    }
-    else if(value === 'b'){
+    } else if (value === 'b') {
       $scope.display = 'Business News';
 
-    }
-
-    else if(value === 'h'){
+    } else if (value === 'h') {
       $scope.display = 'Headlines';
 
-    }
-
-    else if(value === 't'){
+    } else if (value === 't') {
       $scope.display = 'Science and Technology';
 
     }
@@ -54,21 +47,21 @@ textApp.controller('textController', ['$scope', 'myFactory', 'myFactory1', funct
     myFactory.getResponse(value, query, callback);
 
   };
-  
-  $scope.getValue1 = function(){
+
+  $scope.getValue1 = function() {
     if (query < 63) {
       query = query + 8;
 
     } else {
-      query = 0;
+      $scope.show1 = 'No more stories';
     }
     // console.log(query);
-     myFactory.getResponse($scope.value, query, callback);
+    myFactory.getResponse($scope.value, query, callback);
 
   };
 
-   myFactory1.getResponse1(function(data) {
-   	console.log(data);
+  myFactory1.getResponse1(function(data) {
+    console.log(data);
     $scope.show = data;
   });
 
@@ -81,10 +74,23 @@ textApp.controller('textController', ['$scope', 'myFactory', 'myFactory1', funct
 }]);
 
 
-$(document).ready(function(){
+$(document).ready(function() {
   $('.slider').slider({
     full_width: false,
-    interval:5000,
-    transition:800,
+    interval: 5000,
+    transition: 800,
   });
+
+  $('.menu').click(function() {
+    $('.menu').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  // $(window).load(function() {
+    $('ul > li:first-child div div a').addClass('active');
+
+  // });
+
 });
+
+
